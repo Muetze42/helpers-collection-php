24cd6181d243785bd7d59526971cb31bdfd603ca
Norman Huth <norman@huth.it>
2023-12-11 09:27:35 +0100

    feat: add country, currency, language and locale enum

--- src/Traits/EnumTrait.php
+++ src/Traits/EnumTrait.php
@@ -9,9 +9,22 @@ trait EnumTrait
     /**
      * Return value label array.
      *
+     * @deprecated
      * @return array<string, string>
      */
     public static function toOptionsArray(): array
+    {
+        return Arr::mapWithKeys(self::cases(), function (self $enum) {
+            return [$enum->name => $enum->value];
+        });
+    }
+
+    /**
+     * Return value label array.
+     *
+     * @return array<string, string>
+     */
+    public static function toOptionArray(): array
     {
         return Arr::mapWithKeys(self::cases(), function (self $enum) {
             return [$enum->name => $enum->value];
